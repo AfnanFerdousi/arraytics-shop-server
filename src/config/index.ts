1854d93c0240f45122e333004c006598c0f35491
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-12-05 16:03:22 +0600

    [DONE] initial setup

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
+    env: process.env.NODE_ENV,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    jwt: {
+        jwt_secret: process.env.JWT_SECRET,
+        jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+        expires_in: process.env.JWT_EXPIRES_IN,
+        refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    },
+};
